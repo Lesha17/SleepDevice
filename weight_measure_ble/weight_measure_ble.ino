cfa3a9943fa8a8a6d9e503d3d68f6db503db3df5
17aleshka <17aleshka@gmail.com>
2018-04-14 21:24:21 +0300

    Добавил дополнительные характеристики

--- weight_measure_ble/weight_measure_ble.ino
+++ weight_measure_ble/weight_measure_ble.ino
@@ -1,23 +1,5 @@
 /*
-    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-
-   Create a BLE server that, once we receive a connection, will send periodic notifications.
-   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
-   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE" 
-   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
-
-   The design of creating the BLE server is:
-   1. Create a BLE Server
-   2. Create a BLE Service
-   3. Create a BLE Characteristic on the Service
-   4. Create a BLE Descriptor on the characteristic
-   5. Start the service.
-   6. Start advertising.
-
-   In this example rxValue is the data received (only accessible inside that function).
-   And txValue is the data to be sent, in this example just a byte incremented every second. 
+    This sketch configures HX711 chip, measures weight data from it, then sends it to BLE client.
 */
 #include <BLEDevice.h>
 #include <BLEServer.h>
@@ -25,62 +7,28 @@
 #include <BLE2902.h>
 #include "HX711.h"
 
-
-/*
- Example using the SparkFun HX711 breakout board with a scale
- By: Nathan Seidle
- SparkFun Electronics
- Date: November 19th, 2014
- License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
-
- This is the calibration sketch. Use it to determine the calibration_factor that the main example uses. It also
- outputs the zero_factor useful for projects that have a permanent mass on the scale in between power cycles.
-
- Setup your scale and start the sketch WITHOUT a weight on the scale
- Once readings are displayed place the weight on the scale
- Press +/- or a/z to adjust the calibration_factor until the output readings match the known weight
- Use this calibration_factor on the example sketch
-
- This example assumes pounds (lbs). If you prefer kilograms, change the Serial.print(" lbs"); line to kg. The
- calibration factor will be significantly different but it will be linearly related to lbs (1 lbs = 0.453592 kg).
-
- Your calibration factor may be very positive or very negative. It all depends on the setup of your scale system
- and the direction the sensors deflect from zero state
- This example code uses bogde's excellent library: https://github.com/bogde/HX711
- bogde's library is released under a GNU GENERAL PUBLIC LICENSE
- Arduino pin 2 -> HX711 CLK
- 3 -> DOUT
- 5V -> VCC
- GND -> GND
-
- Most any pin on the Arduino Uno will be compatible with DOUT/CLK.
-
- The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
-
-*/
-
 #define DOUT  5
 #define CLK  4
 
-HX711 * scale;
+// https://www.uuidgenerator.net/ UUID generator is used
+#define SERVICE_UUID                         "a22b1352-4007-11e8-b467-0ed5f89f718b" // service UUID
+#define CHARACTERISTIC_UUID_SENSOR_VALUE     "a22b15dc-4007-11e8-b467-0ed5f89f718b"
+#define CHARACTERISTIC_UUID_ON_BED_VALUE     "a22b1730-4007-11e8-b467-0ed5f89f718b"
+#define CHARACTERISTIC_UUID_NOT_ON_BED_VALUE "a22b1852-4007-11e8-b467-0ed5f89f718b"
 
+HX711 * scale;
 float calibration_factor = -7050; //-7050 worked for my 440lb max scale setup
 
-
-BLECharacteristic *pCharacteristic;
+BLECharacteristic *sensorValueCharacteristic;
+BLECharacteristic *onBedSensorValueCharacteristic;
+BLECharacteristic *notOnBedSensorValueCharacteristic;
 bool deviceConnected = false;
-float txValue = 0;
-const int readPin = 32; // Use GPIO number. See ESP32 board pinouts
-const int LED = 2; // Could be different depending on the dev board. I used the DOIT ESP32 dev board.
 
-//std::string rxValue; // Could also make this a global var to access it in loop()
-
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-
-#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
-#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
-#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+typedef union
+{
+ float number;
+ uint8_t bytes[4];
+} FLOATUNION_t;
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -105,39 +53,14 @@ class MyCallbacks: public BLECharacteristicCallbacks {
         for (int i = 0; i < rxValue.length(); i++) {
           Serial.print(rxValue[i]);
         }
-
-        Serial.println();
-
-        // Do stuff based on the command received from the app
-        if (rxValue.find("A") != -1) { 
-          Serial.print("Turning ON!");
-         // digitalWrite(LED, HIGH);
-        }
-        else if (rxValue.find("B") != -1) {
-          Serial.print("Turning OFF!");
-          //digitalWrite(LED, LOW);
-        }
-
-        Serial.println();
+        
         Serial.println("*********");
       }
     }
 };
 
-typedef union
-{
- float number;
- uint8_t bytes[4];
-} FLOATUNION_t;
-
 void setup() {
   Serial.begin(115200);
-
-  Serial.println("HX711 calibration sketch");
-  Serial.println("Remove all weight from scale");
-  Serial.println("After readings begin, place known weight on scale");
-  Serial.println("Press + or a to increase calibration factor");
-  Serial.println("Press - or z to decrease calibration factor");
 
   scale = new HX711(DOUT, CLK);
 
@@ -148,32 +71,35 @@ void setup() {
   Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
   Serial.println(zero_factor);
 
-  //pinMode(LED, OUTPUT);
+  BLEDevice::init("Sleep monitoring device"); // Give it a name
 
-  // Create the BLE Device
-  BLEDevice::init("ESP32 UART Test"); // Give it a name
-
-  // Create the BLE Server
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
-  // Create the BLE Service
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
-  // Create a BLE Characteristic
-  pCharacteristic = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID_TX,
-                      BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ 
+  // Sensor value characteristic
+  sensorValueCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_SENSOR_VALUE,
+                      BLECharacteristic::PROPERTY_NOTIFY
                     );
-                      
-  pCharacteristic->addDescriptor(new BLE2902());
+  sensorValueCharacteristic->addDescriptor(new BLE2902());
 
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID_RX,
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
+  // OnBed sensor value characteristic
+  onBedSensorValueCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_ON_BED_VALUE,
+                      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
+                    );     
+  onBedSensorValueCharacteristic->addDescriptor(new BLE2902());
+  onBedSensorValueCharacteristic->setCallbacks(new MyCallbacks());
 
-  pCharacteristic->setCallbacks(new MyCallbacks());
+// NotOnBed sensor value characteristic
+  notOnBedSensorValueCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_NOT_ON_BED_VALUE,
+                      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
+                    );     
+  notOnBedSensorValueCharacteristic->addDescriptor(new BLE2902());
+  notOnBedSensorValueCharacteristic->setCallbacks(new MyCallbacks());
 
   // Start the service
   pService->start();
@@ -192,30 +118,14 @@ void loop() {
   Serial.print(" calibration_factor: ");
   Serial.print(calibration_factor);
   Serial.println();
-
-  if(Serial.available())
-  {
-    char temp = Serial.read();
-    if(temp == '+' || temp == 'a')
-      calibration_factor += 10;
-    else if(temp == '-' || temp == 'z')
-      calibration_factor -= 10;
-  }
-    // Fabricate some arbitrary junk for now...
-    //txValue = scale_units; // This could be an actual sensor reading!
-
+  
     // Let's convert the value to a char array:
-    //char txString[4]; // make sure this is big enuffz
     FLOATUNION_t txValue;
     txValue.number = scale_units; // Assign a number to the float
     
-    //dtostrf(txValue, 1, 2, txString); // float_val, min_width, digits_after_decimal, char_buffer
+    sensorValueCharacteristic->setValue(txValue.bytes, 4);  
+    sensorValueCharacteristic->notify(); // Send the value to the app!
     
-//    pCharacteristic->setValue(&txValue, 1); // To send the integer value
-//    pCharacteristic->setValue("Hello!"); // Sending a test message
-    pCharacteristic->setValue(txValue.bytes, 4);
-    
-    pCharacteristic->notify(); // Send the value to the app!
     Serial.print("*** Sent Value: ");
     Serial.print(txValue.number);
     Serial.println(" ***");
